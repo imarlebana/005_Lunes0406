7b076fd39b4b1133e9d92af0fd55ce3a81e67726
cice <cice@A8POVMac11.local>
2018-06-04 21:29:55 +0200

    ejercicios

--- src/es/cice/Ejercicio5.java
+++ src/es/cice/Ejercicio5.java
@@ -6,7 +6,7 @@ public class Ejercicio5 {
 
     public static void main(String[] args) {
 
-        int[] numeros = {0,1,2,3};
+        int[] numeros = {0,1,2,3,4,5,6,7,8,9};
         int num =0;
         for(int n : numeros){
             num+=n;
---
+++ src/es/cice/Ejercicio6.java
@@ -0,0 +1,21 @@
+package es.cice;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+
+        int[] numeros = {0,1,2,3,4,5,-6,7,-8,9};
+
+        int i=0;
+
+        while(numeros[i]>=0){
+            ++i;
+        }
+
+        System.out.println("El primer numero negativo esta en la posicion: "  + (++i));
+
+
+       
+
+    }
+}
